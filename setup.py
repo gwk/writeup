3005f33ffa920d13aee7361459a8729d1d1a9ccc
George King <george.w.king@gmail.com>
2017-01-10 10:38:20 -0500

    setup.py: remove long_description.

--- setup.py
+++ setup.py
@@ -9,16 +9,6 @@
 from setuptools import setup
 
 
-long_description = '''\
-Writeup is a text markup format that translates to HTML5. It is similar to Markdown in spirit, but is not equivalent. Some differences:
-- the line-oriented syntax is designed to make correct implementation simple and unambiguous.
-- hierarchical sections.
-- special handling of license lines at the beginning of a file.
-- builtin javascript presentation mode.
-- unambiguous character escape syntaxes.
-- versioning scheme will allow for format evolution while preserving the ability to build old source files.
-'''
-
 setup(
   name='writeup-tool',
   license='CC0',
@@ -27,10 +17,11 @@ setup(
   author_email='george.w.king@gmail.com',
   url='https://github.com/gwk/writeup',
   description='Writeup is a text markup format and standalone tool that translates to HTML5.',
-  long_description=long_description,
   packages=['writeup'],
   entry_points = {'console_scripts': ['writeup=writeup.v0:main']},
-  keywords=['documentation', 'markup'],
+  keywords=[
+    'documentation', 'markup'
+  ],
   classifiers=[ # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     'Development Status :: 3 - Alpha',
     'Environment :: Console',
